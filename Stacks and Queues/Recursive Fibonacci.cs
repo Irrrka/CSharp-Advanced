57e44406a0fe380000ecae58ded094aad9c64bf2
Irrrka <irrrka@gmail.com>
2017-06-05 17:21:13 +0300

    77/100

--- Stacks and Queues/Recursive Fibonacci.cs
+++ Stacks and Queues/Recursive Fibonacci.cs
@@ -1,34 +1,25 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Fibonacci
+namespace Parentheses
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int n = int.Parse(Console.ReadLine());
-            Stack<long> fibonacc = new Stack<long>();
-            fibonacc.Push(1);
-            fibonacc.Push(1);
-            if (n != 0 && n != 1)
+            long n = long.Parse(Console.ReadLine());
+            long FibbNumber = getFibonacci(n);
+            Console.WriteLine(FibbNumber);
+        }
+
+        private static long getFibonacci(long n)
+        {
+            if (n==1 || n==2)
             {
-                for (int i = 2; i < n; i++)
-                {
-                    long n1 = fibonacc.Pop();
-                    long n2 = fibonacc.Peek();
-                    long sum = n1 + n2;
-                    fibonacc.Push(n1);
-                    fibonacc.Push(sum);
-                }
-                Console.WriteLine(fibonacc.Peek());
+                return 1;
             }
             else
             {
-                Console.WriteLine(1);
+                return getFibonacci(n - 1) + getFibonacci(n - 2);
             }
         }
     }
