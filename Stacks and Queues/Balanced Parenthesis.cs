240daae7f330d5da10210d14971735848f5a86f9
Irrrka <irrrka@gmail.com>
2017-06-05 17:10:08 +0300

    100/100

--- Stacks and Queues/Balanced Parenthesis.cs
+++ Stacks and Queues/Balanced Parenthesis.cs
@@ -1,45 +1,66 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace Parentheses
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            string parentheses = Console.ReadLine();
             //{[()]}
+            string input = Console.ReadLine();
+            Stack<char> parentheses = new Stack<char>();
+            bool balanced = false;
 
-            bool isBalanced = false;
-
-            if (parentheses.Length % 2 == 0)
+            for (int i = 0; i < input.Length; i++)
             {
-                Stack<char> left = new Stack<char>();
-                Queue<char> right = new Queue<char>();
-
-                for (int i = 0; i < parentheses.Length; i++)
+                char parenth = input[i];
+                switch (input[i])
                 {
-                    left.Push(parentheses[i]); //{[()]}
-                    right.Enqueue(parentheses[i]); //{[()]}
+                    case '{':
+                        parentheses.Push(parenth);
+                        break;
+                    case '}':
+                        if (parentheses.Count!=0 && parentheses.Peek() == '{')
+                        {
+                            parentheses.Pop();
+                            balanced = true;
+                        }
+                        else
+                            balanced = false;
+                        break;
+                    case '[':
+                        parentheses.Push(parenth);
+                        break;
+                    case ']':
+                        if (parentheses.Count != 0 && parentheses.Peek() == '[')
+                        {
+                            parentheses.Pop();
+                            balanced = true;
+                        }
+                        else
+                            balanced = false;
+                        break;
+                    case '(':
+                        parentheses.Push(parenth);
+                        break;
+                    case ')':
+                        if (parentheses.Count != 0 && parentheses.Peek() == '(')
+                        {
+                            parentheses.Pop();
+                            balanced = true;
+                        }
+                        else
+                            balanced = false;
+                        break;
+                    default:
+                        break;
                 }
- 	           while (right.Count > parentheses.Length / 2)
-                {
-                    char currPerentheseinStack = left.Pop();
-                    char currParentheseinQueue = right.Dequeue();
-                    if (currPerentheseinStack == currParentheseinQueue+2 || currPerentheseinStack == currParentheseinQueue - 2)
-                        isBalanced = true;
-                    else if (currPerentheseinStack == currParentheseinQueue + 1 || currPerentheseinStack == currParentheseinQueue - 1)
-                        isBalanced = true;
-                }
-                if (isBalanced)
-                    Console.WriteLine("YES");
             }
-
-            else Console.WriteLine("NO");
-            
+            if (balanced)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
         }
     }
 }
